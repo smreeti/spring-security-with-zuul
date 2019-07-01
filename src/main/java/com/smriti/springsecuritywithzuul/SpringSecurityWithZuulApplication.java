5fa0d7684184a057918594881e3bc19d743d9227
smriti.mool <smriti.mool@f1soft.com>
2019-07-01 11:45:20 +0545

    Initial commit

---
+++ src/main/java/com/smriti/springsecuritywithzuul/SpringSecurityWithZuulApplication.java
@@ -0,0 +1,13 @@
+package com.smriti.springsecuritywithzuul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityWithZuulApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityWithZuulApplication.class, args);
+	}
+
+}
