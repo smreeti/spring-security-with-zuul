6087d35e1530d98e0f31a085e50226c482f4a13c
Sauravi Thapa <sauravi.thapa@f1soft.com>
2019-07-05 15:44:43 +0545

    department-setup

---
+++ department-service/src/main/java/com/f1soft/departmentservice/DepartmentServiceApplication.java
@@ -0,0 +1,13 @@
+package com.f1soft.departmentservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DepartmentServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DepartmentServiceApplication.class, args);
+	}
+
+}
