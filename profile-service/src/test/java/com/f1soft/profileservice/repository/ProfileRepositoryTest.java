4355af191785e6d84984cc7ffe5688a517b0a932
smriti.mool <smriti.mool@f1soft.com>
2019-07-13 21:27:57 +0545

    add repository test for update profile

--- profile-service/src/test/java/com/f1soft/profileservice/repository/ProfileRepositoryTest.java
+++ profile-service/src/test/java/com/f1soft/profileservice/repository/ProfileRepositoryTest.java
@@ -1,6 +1,7 @@
 package com.f1soft.profileservice.repository;
 
 import com.f1soft.profileservice.entities.Profile;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,7 +10,10 @@ import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.math.BigInteger;
+
 import static com.f1soft.profileservice.utils.ProfileRequestUtils.getProfileInfo;
+import static java.math.BigInteger.*;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.Assert.assertNotNull;
 
@@ -23,6 +27,10 @@ public class ProfileRepositoryTest {
 
     @Autowired
     private TestEntityManager testEntityManager;
+
+    @Autowired
+    private ProfileRepository profileRepository;
+
 
     @Test
     public void saveProfile() {
@@ -53,6 +61,30 @@ public class ProfileRepositoryTest {
         assertNotNull(result);
 
         assertThat(profile.getName()).isEqualTo(getProfileInfo().getName());
+    }
+
+    @Test
+    public void fetchProfileByName_Should_Return_0() {
+        BigInteger result = profileRepository.findProfileByName("F1soft");
+        assertThat(result).isEqualTo(ZERO);
+    }
+
+    @Test
+    public void fetchProfileByName_Should_Return_1() {
+        BigInteger result = profileRepository.findProfileByName("F1soft Profile");
+        assertThat(result).isEqualTo(ONE);
+    }
+
+    @Test
+    public void fetchProfileByIdAndName_Should_Return_0() {
+        BigInteger result = profileRepository.findProfileByIdAndName(1L, "F1soft");
+        assertThat(result).isEqualTo(ZERO);
+    }
+
+    @Test
+    public void fetchProfileByIdAndName_Should_Return_1() {
+        BigInteger result = profileRepository.findProfileByIdAndName(2L, "F1soft Profile");
+        assertThat(result).isEqualTo(ONE);
     }
 
 
---
+++ profile-service/src/test/java/com/f1soft/profileservice/repository/sac.java
@@ -0,0 +1,57 @@
+package com.f1soft.profileservice.repository;
+
+/**
+ * @author smriti on 7/13/19
+ */
+//public class sac {
+//    @Autowired
+//    private TestEntityManager testEntityManager;
+//
+//    @Autowired
+//    private DepartmentRepository departmentRepository;
+//
+//    @Test
+//    public void testSaveDepartment() {
+//        Department departmentToSave = getDepartmentInfo();
+//        Department department = testEntityManager.persist(departmentToSave);
+//        System.out.println(department);
+//        assertThat(department).isNotNull();
+//    }
+//
+//    @Test
+//    public void testfetchAllDepartment() {
+//        List<Department> departmentList = departmentRepository.fetchAllDepartment();
+//        System.out.println(departmentList);
+//        assertThat(departmentList).hasSize(2);
+//    }
+//
+//    @Test
+//    public void testfindByName() {
+//        Department departmentSavedInDb = departmentRepository.findByName(getDepartmentInfo().getDepartmentName());
+//
+//        assertThat(departmentSavedInDb.getDepartmentName()).isEqualTo(getDepartmentInfo().getDepartmentName());
+//    }
+//
+//    @Test
+//    public void testfindByCode() {
+//        Department departmentSavedInDb = departmentRepository.findByCode(getDepartmentInfo().getCode());
+//
+//        assertThat(departmentSavedInDb.getCode()).isEqualTo(getDepartmentInfo().getCode());
+//    }
+//
+//    @Test
+//    public void testfindByDepartmentId() {
+//        Department departmentSavedInDb = departmentRepository.findByDepartmentId(31L);
+//        System.out.println(departmentSavedInDb);
+//        assertThat(departmentSavedInDb.getDepartmentName()).isEqualTo(getDepartmentInfo().getDepartmentName());
+//        assertThat(departmentSavedInDb.getCode()).isEqualTo(getDepartmentInfo().getCode());
+//    }
+//
+//    @Test
+//    public void testsearchDepartment() {
+//        Department departmentSavedInDb = departmentRepository.searchDepartment(31L, getDepartmentInfo().getDepartmentName(), getDepartmentInfo().getCode(), getDepartmentInfo().getStatus());
+//
+//        assertThat(departmentSavedInDb).isNotNull();
+//    }
+
+//}
